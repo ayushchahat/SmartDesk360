e7ee8fa2a9b99df830b4c441a7b99fb0c53ad9b0
Cristianodhoni09 <amitkum9898@gmail.com>
2025-01-04 16:07:55 +0530

    Implement routing and layout structure in App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
-
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import InventoryPage from './pages/InventoryPage';
+import BillingPage from './pages/BillingPage';
+import ReturnPage from './pages/ReturnPage';
+import Navbar from './components/Layout/Navbar';
 
 function App() {
   return (
-    <div> Hello world</div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/inventory" element={<InventoryPage />} />
+        <Route path="/billing" element={<BillingPage />} />
+        <Route path="/returns" element={<ReturnPage />} />
+      </Routes>
+    </Router>
   );
 }
 
