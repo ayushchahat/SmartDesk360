ae7b76fffeb9bc2844fb3bb33e3bff481338526d
Cristianodhoni09 <amitkum9898@gmail.com>
2025-01-04 16:22:56 +0530

    Add ExpiryNotification component for displaying expiry notifications

---
+++ frontend/src/components/Inventory/ExpiryNotification.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ExpiryNotification = () => {
+  return (
+    <div>
+      This is expiry notification page.
+    </div>
+  )
+}
+
+export default ExpiryNotification
