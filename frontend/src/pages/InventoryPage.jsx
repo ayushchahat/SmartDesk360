f02a71f0a9314ae2c3a6a86eb1871ed9d0c19bf8
Cristianodhoni09 <amitkum9898@gmail.com>
2025-01-04 16:16:01 +0530

    Add InventoryPage component to display InventoryForm

---
+++ frontend/src/pages/InventoryPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import InventoryForm from '../components/Inventory/InventoryForm';
+
+const InventoryPage = () => {
+  return (
+    <div>
+      <InventoryForm />
+    </div>
+  );
+};
+
+export default InventoryPage;
