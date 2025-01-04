6ecb0cafbddb3e5fea859473cb400aef61aedb2d
Cristianodhoni09 <amitkum9898@gmail.com>
2025-01-04 16:20:22 +0530

    Add ReturnPage component to render ReturnForm

---
+++ frontend/src/pages/ReturnPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReturnForm from '../components/Inventory/ReturnForm';
+
+const ReturnPage = () => {
+  return (
+    <div>
+      <ReturnForm />
+    </div>
+  );
+};
+
+export default ReturnPage;
